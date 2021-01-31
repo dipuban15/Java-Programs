ae175289e5c211d859ced6fe64b390022c830310
BlazingObsidian <74757575+BlazingObsidian@users.noreply.github.com>
2021-01-31 08:43:52 +0530

    SumOfNum
    Calculates sum of two numbers entered by user

--- SumOfNum.java
+++ SumOfNum.java
@@ -1,2 +1,12 @@
 import java.util.Scanner;
 class SumOfNum{
+  public static void main()
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.print("\n Enter 2 numbers");
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int sum=a+b;
+    System.out.print("\n Sum of the 2 numbers is "+sum);
+  }
+}
