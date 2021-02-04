820d5ed92f4a6cc3e228827f2c51fa146c7c5798
DIPTAYAN BANERJEE <78288177+dipuban15@users.noreply.github.com>
2021-02-04 12:00:39 +0530

    Create StringOP.java

---
+++ StringOP.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+class StringOP{
+  public static void main(){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("\n Enter a string");
+    String st=sc.nextLine();
+    
